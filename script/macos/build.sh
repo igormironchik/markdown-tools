52ca2f8a78d987aaaac75150944e80a886e3c1d5
Igor Mironchik <igor.mironchik@gmail.com>
2024-03-21 15:51:31 +0300

    Quote prefix path on MacOS.

--- script/macos/build.sh
+++ script/macos/build.sh
@@ -2,6 +2,6 @@ echo "Building markdown-tools..."
 
 mkdir build-markdown-tools
 
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B build-markdown-tools -DBUILD_MDPDF_TESTS=OFF -DCMAKE_PREFIX_PATH=$PWD/Qt/6.6.2/macos:`brew --prefix` -DECM_DIR=$PWD/../KDE/share/ECM/cmake -DKF6SyntaxHighlighting_DIR=$PWD/../KDE/lib/cmake/KF6SyntaxHighlighting -DFontconfig_INCLUDE_DIR=`brew --prefix fontconfig`/include -DOPENSSL_ROOT_DIR=`brew --prefix openssl@3` || exit 1
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B build-markdown-tools -DBUILD_MDPDF_TESTS=OFF -DCMAKE_PREFIX_PATH="$PWD/Qt/6.6.2/macos:`brew --prefix`" -DECM_DIR=$PWD/../KDE/share/ECM/cmake -DKF6SyntaxHighlighting_DIR=$PWD/../KDE/lib/cmake/KF6SyntaxHighlighting -DFontconfig_INCLUDE_DIR=`brew --prefix fontconfig`/include -DOPENSSL_ROOT_DIR=`brew --prefix openssl@3` || exit 1
 
 cmake --build build-markdown-tools --config Release || exit 1
