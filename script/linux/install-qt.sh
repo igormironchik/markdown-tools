1e93e2cf40e4e5d01caa978f80d82ae80f45c272
Igor Mironchik <igor.mironchik@gmail.com>
2024-08-14 21:13:28 +0300

    Fix arch for aqt installer on Linux.

--- script/linux/install-qt.sh
+++ script/linux/install-qt.sh
@@ -6,4 +6,4 @@ mkdir Qt
 
 echo "Installing Qt..."
 
-aqt install-qt --outputdir ./Qt linux desktop 6.7.2 gcc_64 -m qtimageformats qtwebchannel qtwebengine qtwebview qtpositioning || exit 1
+aqt install-qt --outputdir ./Qt linux desktop 6.7.2 linux_gcc_64 -m qtimageformats qtwebchannel qtwebengine qtwebview qtpositioning || exit 1
