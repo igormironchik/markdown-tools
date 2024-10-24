5220d211527ee56d7e548cf45bdadcaf73eebc25
Igor Mironchik <igor.mironchik@gmail.com>
2024-10-24 13:55:03 +0300

    Use debug build in testing workflow.

--- script/linux/build.sh
+++ script/linux/build.sh
@@ -4,6 +4,12 @@ mkdir build-markdown-tools
 
 qt_version=$(cat $PWD/script/qt.version)
 
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B build-markdown-tools -DBUILD_MDPDF_TESTS=OFF -DCMAKE_PREFIX_PATH=$PWD/Qt/$qt_version/gcc_64 -DECM_DIR=$PWD/../KDE/share/ECM/cmake -DKF6SyntaxHighlighting_DIR=$PWD/../KDE/lib/x86_64-linux-gnu/cmake/KF6SyntaxHighlighting || exit 1
+build_type = $1
+
+if [ -z "$1" ] then
+    build_type="Release"
+fi
+
+cmake -DCMAKE_BUILD_TYPE=$build_type -S . -B build-markdown-tools -DBUILD_MDPDF_TESTS=OFF -DCMAKE_PREFIX_PATH=$PWD/Qt/$qt_version/gcc_64 -DECM_DIR=$PWD/../KDE/share/ECM/cmake -DKF6SyntaxHighlighting_DIR=$PWD/../KDE/lib/x86_64-linux-gnu/cmake/KF6SyntaxHighlighting || exit 1
 
 cmake --build build-markdown-tools --config Release || exit 1
