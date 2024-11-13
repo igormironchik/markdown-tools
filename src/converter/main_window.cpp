c693e208cfd58e4aef826bf6840dbc0e3385d9c1
Igor Mironchik <igor.mironchik@gmail.com>
2024-11-13 17:42:58 +0300

    Add licensing information.

--- src/converter/main_window.cpp
+++ src/converter/main_window.cpp
@@ -1734,6 +1734,31 @@ void MainWindow::licenses()
                                   "<p>This Source Code Form is \"Incompatible With Secondary Licenses\", as "
                                   "defined by the Mozilla Public License, v. 2.0.</p>"));
 
+    msg.addLicense(QStringLiteral("MicroTeX"),
+                   QStringLiteral("<p><b>MicroTeX license</b></p>\n"
+                                  "<p>The MIT License (MIT)<</p>\n"
+                                  "\n"
+                                  "<p>Copyright (c) 2020 Nano Michael</p>\n"
+                                  "\n"
+                                  "<p>Permission is hereby granted, free of charge, to any person obtaining a copy "
+                                  "of this software and associated documentation files (the \"Software\"), to deal "
+                                  "in the Software without restriction, including without limitation the rights "
+                                  "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell "
+                                  "copies of the Software, and to permit persons to whom the Software is "
+                                  "furnished to do so, subject to the following conditions:</p>\n"
+                                  "\n"
+                                  "<p>The above copyright notice and this permission notice shall be included in all "
+                                  "copies or substantial portions of the Software.</p>\n"
+                                  "\n"
+                                  "<p>THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
+                                  "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
+                                  "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE "
+                                  "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
+                                  "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
+                                  "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE "
+                                  "SOFTWARE.</p>"));
+
+
     msg.exec();
 }
 
