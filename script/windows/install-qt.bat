3693ae0da762dbfbb289f5d6ded1bf11c6e29fb6
Igor Mironchik <igor.mironchik@gmail.com>
2024-11-14 13:06:28 +0300

    Revert to Qt version 6.7.3.

--- script/windows/install-qt.bat
+++ script/windows/install-qt.bat
@@ -8,7 +8,7 @@ echo "Installing Qt..."
 
 set /P qt_version=<%CD%\script\qt.version
 
-aqt install-qt --outputdir Qt windows desktop %qt_version% win64_msvc2019_64 -m qtimageformats qtwebchannel qtwebview qtpositioning
+aqt install-qt --outputdir Qt windows desktop %qt_version% win64_msvc2019_64 -m qtimageformats qtwebchannel qtwebview qtpositioning qtwebengine
 
 IF %ERRORLEVEL% NEQ 0 (
 	exit /B %ERRORLEVEL%
