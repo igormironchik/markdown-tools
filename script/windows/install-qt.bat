88d8b8fd303f9380720fa0ee01f2e9b491069015
Igor Mironchik <igor.mironchik@gmail.com>
2025-04-12 07:14:13 +0300

    Fix arch for Qt installation on Windows.

--- script/windows/install-qt.bat
+++ script/windows/install-qt.bat
@@ -8,7 +8,7 @@ echo "Installing Qt..."
 
 set /P qt_version=<%CD%\script\qt.version
 
-aqt install-qt --outputdir Qt windows desktop %qt_version% win64_msvc2019_64 -m qtimageformats qtwebchannel qtwebview qtpositioning qtwebengine
+aqt install-qt --outputdir Qt windows desktop %qt_version% win64_msvc2022_64 -m qtimageformats qtwebchannel qtwebview qtpositioning qtwebengine
 
 IF %ERRORLEVEL% NEQ 0 (
 	exit /B %ERRORLEVEL%
