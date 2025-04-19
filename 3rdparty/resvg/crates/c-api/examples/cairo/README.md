9b8a43de7650d4ab5e764105c3c499cef5d424dc
Igor Mironchik <igor.mironchik@gmail.com>
2025-04-19 07:53:56 +0300

    Store resvg library as archive.

--- 3rdparty/resvg/crates/c-api/examples/cairo/README.md
+++
@@ -1,9 +0,0 @@
-A simple example that shows how to use *resvg* through C API to render on a Cairo context.
-
-## Run
-
-```bash
-cargo build --manifest-path ../../Cargo.toml
-make
-LD_LIBRARY_PATH=../../../../target/debug ./example image.svg image.png
-```
--- 3rdparty/resvg/tools/explorer-thumbnailer/src/interfaces/mod.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 the Resvg Authors
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-
-pub mod iinitialize_with_stream;
-pub mod ithumbnail_provider;
-
-pub use iinitialize_with_stream::IInitializeWithStream;
-pub use ithumbnail_provider::IThumbnailProvider;
