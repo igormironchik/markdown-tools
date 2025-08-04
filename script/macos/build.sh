22f6b7c6683e2ff9849211ed50f7b7a989ad8800
Igor Mironchik <igor.mironchik@gmail.com>
2025-08-04 09:17:16 +0300

    Fix build of PoDoFo on MacOS with scripts.

--- script/macos/build.sh
+++ script/macos/build.sh
@@ -4,6 +4,6 @@ qt_version=$(cat $PWD/script/qt.version)
 
 mkdir build-markdown-tools
 
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B build-markdown-tools -DBUILD_MDPDF_TESTS=OFF -DCMAKE_PREFIX_PATH="$PWD/Qt/$qt_version/macos;`brew --prefix`" -DECM_DIR=$PWD/../KDE/share/ECM/cmake -DKF6SyntaxHighlighting_DIR=$PWD/../KDE/lib/cmake/KF6SyntaxHighlighting -DKF6Sonnet_DIR=$PWD/../KDE/lib/cmake/KF6Sonnet -DFontconfig_INCLUDE_DIR=`brew --prefix fontconfig`/include -DOPENSSL_ROOT_DIR=`brew --prefix openssl@3` || exit 1
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B build-markdown-tools -DCMAKE_FIND_FRAMEWORK=NEVER -DBUILD_MDPDF_TESTS=OFF -DCMAKE_PREFIX_PATH="$PWD/Qt/$qt_version/macos;`brew --prefix`" -DECM_DIR=$PWD/../KDE/share/ECM/cmake -DKF6SyntaxHighlighting_DIR=$PWD/../KDE/lib/cmake/KF6SyntaxHighlighting -DKF6Sonnet_DIR=$PWD/../KDE/lib/cmake/KF6Sonnet -DFontconfig_INCLUDE_DIR=`brew --prefix fontconfig`/include -DOPENSSL_ROOT_DIR=`brew --prefix openssl@3` || exit 1
 
 cmake --build build-markdown-tools --config Release || exit 1
