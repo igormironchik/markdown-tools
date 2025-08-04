0168be9ab309db862da00a62b07862a725b1ea9b
Igor Mironchik <igor.mironchik@gmail.com>
2025-08-04 15:45:55 +0300

    Add option to build with systems' libpodofo and libresvg.

---
+++ cmake/Findresvg.cmake
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2025 Igor Mironchik <igor.mironchik@gmail.com>
+# SPDX-License-Identifier: MIT
+
+if(NOT RESVG_INCLUDE_DIR)
+    find_path(RESVG_INCLUDE_DIR NAMES resvg.h ResvgQt.h)
+endif()
+
+find_library(RESVG_LIBRARY NAMES resvg)
+
+if(RESVG_INCLUDE_DIR AND RESVG_LIBRARY)
+    set(RESVG_FOUND TRUE)
+    set(resvg_FOUND TRUE)
+	
+    if(NOT TARGET resvg)
+        add_library(resvg UNKNOWN IMPORTED)
+
+        target_include_directories(resvg INTERFACE ${RESVG_INCLUDE_DIR})
+        set_target_properties(resvg PROPERTIES
+            IMPORTED_LOCATION ${RESVG_LIBRARY})
+    endif()
+endif ()
