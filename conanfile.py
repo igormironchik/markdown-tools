8a2f880cd590bc9175e4c0f62944b0c345b95df0
Igor Mironchik <igor.mironchik@gmail.com>
2026-07-21 09:48:10 +0300

    ...

--- conanfile.py
+++ conanfile.py
@@ -7,10 +7,15 @@ class DependenciesRecipe(ConanFile):
     def requirements(self):
         self.requires("zlib/1.3.1")
         self.requires("hunspell/1.7.2")
+        self.requires("tinyxml2/10.0.0")
+        if self.settings.os == "Windows":
+            self.requires("fontconfig/2.15.0")
 
     def configure(self):
         self.options["zlib"].shared = False
         self.options["hunspell"].shared = True
+        self.options["tinyxml2"].shared = False
+        self.options["fontconfig"].shared = False
 
     def imports(self):
         self.copy("*.dll", "bin", "bin")
