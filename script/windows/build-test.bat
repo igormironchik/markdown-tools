4de6203e7065b98f31b28b2ad2e310f0fba44552
Igor Mironchik <igor.mironchik@gmail.com>
2026-07-27 07:12:32 +0300

    Fix building of test.render test on Windows (but because of different font managers test on Windows fails with metrics done on Linux)

---
+++ script/windows/build-test.bat
@@ -0,0 +1,20 @@
+set /P qt_version=<%CD%\script\qt.version
+set /P qt_arch=<%CD%\script\qt.arch.win
+
+set "current_dir=%CD%"
+
+set "cwd=%current_dir:\=/%"
+
+echo "Building markdown-tools..."
+
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B "%cwd%/../builds/build-markdown-tools-test" -DBUILD_MD_TESTS=ON -DCMAKE_MODULE_PATH="%cwd%/../builds/conan" -DCMAKE_PREFIX_PATH="%cwd%/../Qt/%qt_version%/%qt_arch%;%cwd%/../KDE;%cwd%/../builds/conan" -DECM_DIR="%cwd%/../KDE/share/ECM/cmake" -G "NMake Makefiles"
+
+IF %ERRORLEVEL% NEQ 0 (
+	exit /B %ERRORLEVEL%
+)
+
+cmake --build ..\builds\build-markdown-tools-test --config Release -j 3
+
+IF %ERRORLEVEL% NEQ 0 (
+	exit /B %ERRORLEVEL%
+)
